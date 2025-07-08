71916f0b84320dd21b2688e22f720be267b5a16b
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 21:37:03 +0500

    Add src\pages\Cart.js

---
+++ src\pages\Cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+}
+
+export default Cart;

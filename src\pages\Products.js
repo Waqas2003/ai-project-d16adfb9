342b554af101ac588fe5da5362442239ee2961c3
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 21:37:10 +0500

    Add src\pages\Products.js

---
+++ src\pages\Products.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              {product.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Products;

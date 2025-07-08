d540143ff3e9db269d73c83847089116b762c1d9
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 21:37:08 +0500

    Add src\pages\ProductDetail.js

---
+++ src\pages\ProductDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProductDetail() {
+  return (
+    <div>
+      <h1>Product Detail</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+}
+
+export default ProductDetail;

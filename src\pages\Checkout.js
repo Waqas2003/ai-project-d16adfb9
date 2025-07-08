15dce8f234e23edacbcb8e99ce9850f4fd86bfe3
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 21:37:04 +0500

    Add src\pages\Checkout.js

---
+++ src\pages\Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+}
+
+export default Checkout;

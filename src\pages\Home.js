22723b895539ef7ee01558ce91ec9485ba57db2a
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 21:37:06 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to our ecommerce website!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
